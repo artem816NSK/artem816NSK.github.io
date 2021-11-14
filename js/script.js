37977ab30ee8b3b5d202f62ac7be10d8d92c938f
artem816NSK <artem1bannikov@gmail.com>
2021-11-14 23:47:57 +0300

    1

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+var scene = document.getElementById('scene');
+var parallax = new Parallax(scene);
+
+document.addEventListener('DOMContentLoaded', function () {
+    const slider = new ChiefSlider('.slider', {
+      loop: false
+    });
+  });
+
+  
